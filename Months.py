d6c9ba8d0fd209008fbc1f6c2576e615aedc1f7f
renusree1 <renusree10.n@gmail.com>
2026-01-03 12:53:40 +0400

    Create Months.py

---
+++ Months.py
@@ -0,0 +1,3 @@
+import calendar
+months = calendar.month_name
+print(months[1], months[2], months[3], months[4], months[5], months[6], months[7], months[8], months[9], months[10], months[11], months[12])
