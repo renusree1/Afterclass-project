ae82be5e1436695123d565437d7fb08b6f657721
renusree1 <renusree10.n@gmail.com>
2026-01-03 12:56:23 +0400

    Update Months.py

--- Months.py
+++ Months.py
@@ -1,3 +1,3 @@
 import calendar
 months = calendar.month_name
-print(months[1], months[2], months[3], months[4], months[5], months[6], months[7], months[8], months[9], months[10], months[11], months[12])
+print("\n", months[1], "\n", months[2], "\n", months[3], "\n", months[4], "\n", months[5], "\n", months[6], "\n", months[7], "\n", months[8], "\n", months[9], "\n", months[10], "\n", months[11], "\n", months[12], "\n")
